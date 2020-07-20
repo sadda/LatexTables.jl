ecb35211c9b5f07ddf923d88e62ca91161c82f4a
Vaclav Macha <vaclav.macha92@gmail.com>
2020-07-20 12:55:58 +0200

    Merged indices.jl into utilities.jl

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,3 +1,51 @@
+struct All <: Indices end
+
+struct Cells <: Indices
+    inds::Vector{<:CartesianIndex}
+end
+
+Cells(inds::Tuple{Int, Int}...) = Cells([CartesianIndex.(inds)...])
+
+struct Rows <: Indices
+    inds::Vector{<:Int}
+end
+
+Rows(ind::Int) = Rows([ind])
+Rows(inds::Int...) = Rows([inds...])
+
+struct Cols <: Indices
+    inds::Vector{<:Int}
+end
+
+Cols(ind::Int) = Cols([ind])
+Cols(inds::Int...) = Cols([inds...])
+
+
+function setstyle!(c::Cell, args::LatexStyle...)
+    for arg in args
+        setstyle!(c.style, arg)
+    end
+end
+
+
+function setstyle!(d::Dict{Symbol,LatexStyle}, s::LatexStyle)
+    d[Symbol(lowercase(string(typeof(s))))] = s
+end
+
+
+function setstyle!(M::CellArray, args::LatexStyle...)
+    for cell in M
+        setstyle!(cell, args...)
+    end
+end
+
+
+setstyle!(M::CellMatrix, r::All, args::LatexStyle...) = setstyle!(M, args...)
+setstyle!(M::CellMatrix, r::Cells, args::LatexStyle...) = @views setstyle!(M[r.inds], args...)
+setstyle!(M::CellMatrix, r::Rows, args::LatexStyle...) = @views setstyle!(M[r.inds, :], args...)
+setstyle!(M::CellMatrix, r::Cols, args::LatexStyle...) = @views setstyle!(M[:, r.inds], args...)
+
+
 function make_tex_file(file_name::String, s::String; dir_name::String = pwd())
 
     full_name = joinpath(dir_name, file_name)
